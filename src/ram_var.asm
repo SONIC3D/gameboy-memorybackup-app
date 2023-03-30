55da075b1c72ab39e482712fa9d9845676a592cc
Lu ZongJing <sonic3d@gmail.com>
2023-03-31 04:24:29 +0800

    Added: VBlank related routines.

--- src/ram_var.asm
+++ src/ram_var.asm
@@ -4,7 +4,8 @@ byte_C000::	    ds 1        ; 本周期按下了的按键(这些键的bit被置�
 byte_C001::	    ds 1        ; 上周期没有按下，而本周期按下了的按键(这些键的bit被置为1)
                             ; Bit7/6/5/4/3/2/1/0对应下/上/左/右/Start/Select/B/A
 
-SECTION "VariablesC038", WRAM0[$C038]
+SECTION "VariablesC037", WRAM0[$C037]
+byte_C037::     ds 1        ; 每次VBlank函数发生时，该变量会被设成1
 byte_C038::     ds 1        ; 存放的是当前插的游戏卡带，最后可用的SRAM的Bank编号数量(最后一个可用Bank的序号+1)
 
 SECTION "VariablesC053", WRAM0[$C053]
